690e9c6b3dda51b43925c08882fe90f5aa0114a0
Omotayo Oyeniyi <tayotomioyeniyi@gmail.com>
2019-01-30 21:58:28 +0100

    [Feature#163587283] create political party

---
+++ api/src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json()); 
+
+const version = 'v1';
+
+const parties = [];
+const requiredFields = ['name'];
+
+app.post(`/api/${version}/parties`, (req, res) => {
+    const { body } = req;
+    let isValid = true;
+    const keys = Object.keys(body);
+    requiredFields.forEach((field) => {
+        isValid = isValid && keys.includes(field);
+    });
+    if (isValid) {
+        const id = parties.length + 1;
+        const data = {
+            id,
+            name: body.name,
+            hqAddress: body.hqAddress,
+            logoUrl: body.logoUrl,
+        };
+        parties.push(data);
+        const status = 201;
+        res.status(status).json({
+            status,
+            data,
+        });
+    } else {
+        const missingKeys = requiredFields.filter(key => !Object.keys(body).includes(key));
+        res.status(400).json({
+            status: 400,
+            data: `The ${missingKeys[0]} of the party is missing`,
+        });
+    }
+});
+
+app.listen(3000, () => {
+    
+    console.log('Listening on port 3000');
+});
+
+module.exports = app;
